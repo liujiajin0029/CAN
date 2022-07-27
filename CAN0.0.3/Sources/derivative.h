1d3776bb6368faf96a35618b3fad91a16a3a141a
liujiajin0029 <1054525774@qq.com>
2022-07-27 17:32:52 +0800

    修改忽略文件

---
+++ CAN0.0.3/Sources/derivative.h
@@ -0,0 +1,16 @@
+/*
+ * Note: This file is recreated by the project wizard whenever the MCU is
+ *       changed and should not be edited by hand
+ */
+
+/* Include the derivative-specific header file */
+#include <MC9S12XEP100.h>
+
+typedef unsigned    char 		uint8_t;
+typedef unsigned 	short int 	uint16_t;
+typedef unsigned    int 		uint32_t;
+
+
+
+#pragma LINK_INFO DERIVATIVE "MC9S12XEP100"
+
